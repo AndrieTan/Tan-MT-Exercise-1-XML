7ca0cb2fc170cef8a71fa328a604565376a68602
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-02-01 18:26:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <?php
- $domOBJ = new DOMDocument();
- $domOBJ->load("https://tan-mt-exercise-1-xml.herokuapp.com/rss.php");//XML page URL
- 
- $content = $domOBJ->getElementsByTagName("Games");
- ?>
-<h1> Games </h1>
-    <?php
- foreach($content as $data)
- {
-   $Name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
-   $Genre = $data->getElementsByTagName("Genre")->item(0)->nodeValue;
-   $Cost = $data->getElementsByTagName("Cost")->item(0)->nodeValue;
-   $Rate= $data->getElementsByTagName("Rate")->item(0)->nodeValue;
-   echo "<ul><h2>$Name</h2> 
-            <ul>
-                <li>Genre: $Genre </li>
-		<li>Cost: $Cost </li>
-		<li>Rating: $Rate </li>
-            </ul>
-        </ul>";
- }
+       $domOBJ = new DOMDocument();
+       $domOBJ->load("https://tan-mt-exercise-1-xml.herokuapp.com/rss.php");//XML page URL
+
+       $content = $domOBJ->getElementsByTagName("Games");
 ?>
+    
+       
+       <h1>Monsters</h1>
+           
+       
+        <?php
+              foreach($content as $data)
+        {          
+           $Name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
+           $Genre = $data->getElementsByTagName("Genre")->item(0)->nodeValue;
+           $Cost = $data->getElementsByTagName("Cost")->item(0)->nodeValue;
+           $Rate = $data->getElementsByTagName("Rate")->item(0)->nodeValue;
+           echo "<ul><h2>$Namer</h2>
+                    <ul>
+                        <li>Genre: $Genre </li>
+                        <li>Cost: $Cost</li>
+                        <li>Rate: $Rate</li>
+                        
+                    </ul>
+                </ul>";
+              }
+?>
+    
