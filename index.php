96be701a8f3a5379a894a6faa1d6d9d8c98e653a
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-02-01 18:22:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
     <?php
  foreach($content as $data)
  {
-   $Game_Name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
-   $Game_Genre = $data->getElementsByTagName("Genre")->item(0)->nodeValue;
-   $Game_Cost = $data->getElementsByTagName("Cost")->item(0)->nodeValue;
-   $Game_Rate= $data->getElementsByTagName("Rate")->item(0)->nodeValue;
-   echo "<ul><h2>$Game_Name</h2> 
+   $Name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
+   $Genre = $data->getElementsByTagName("Genre")->item(0)->nodeValue;
+   $Cost = $data->getElementsByTagName("Cost")->item(0)->nodeValue;
+   $Rate= $data->getElementsByTagName("Rate")->item(0)->nodeValue;
+   echo "<ul><h2>$Name</h2> 
             <ul>
-                <li>Genre: $Game_Genre</li>
-		<li>Cost: $Game_Cost</li>
-		<li>Rating: $Game_Rate</li>
+                <li>Genre: $Genre </li>
+		<li>Cost: $Cost </li>
+		<li>Rating: $Rate </li>
             </ul>
         </ul>";
  }
